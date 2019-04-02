c4bbb2343d6fa08487a37353dcd3daf2be428b82
Alexander Archer <aca3936@gmail.com>
2019-04-02 15:06:41 -0400

    ex51

--- gothonweb/tests/test_gothonweb.py
+++ gothonweb/tests/test_gothonweb.py
@@ -1,14 +1,23 @@
-from nose2.tools import *
-import NAME
+from app import app
+import unittest
+
+app.config['TESTING'] = True
+web = app.test_client()
 
 
-def setup():
-    print("SETUP!")
+class test_index(unittest.TestCase):
 
+    def test_index(self):
+        rv = web.get('/', follow_redirects=True)
+        self.assertEqual(rv.status_code, 404)
 
-def teardown():
-    print("TEAR DOWN!")
+    def test_hello_get(self):
+        rv = web.get('/hello', follow_redirects=True)
+        self.assertEqual(rv.status_code, 200)
+        self.assertIn(b"Fill Out This Form", rv.data)
 
-
-def test_basic():
-    print("I RAN!", end='')
+    def test_hello_post(self):
+        data = {'name': 'Zed', 'greet': 'Hola'}
+        rv = web.post('/hello', follow_redirects=True, data=data)
+        self.assertIn(b"Zed", rv.data)
+        self.assertIn(b"Hola", rv.data)
